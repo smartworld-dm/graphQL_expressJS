b5da6425081bd9bf4c5829036859d640bcfdf862
smartworld <smartworld1004>
2018-08-13 17:18:07 +0700

    graphQL - simple query

---
+++ src/index.js
@@ -0,0 +1,47 @@
+const { GraphQLServer } = require('graphql-yoga')
+
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+
+let idCount = links.length
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Mutation: {
+    addLink: (root, args) => {
+       const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    },
+    updateLink: (root, args) => {
+       const link = {
+        id: args.id,
+        description: args.description,
+        url: args.url,
+      }
+      return link
+    },
+    deleteLink: (root, args) => {
+       const link = root
+       console.log(root)
+      if (root.id != args.id)
+      	links.push(link)
+      return link
+    },
+  },
+}
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+})
+server.start(() => console.log(`Server is running on http://localhost:4000`))
